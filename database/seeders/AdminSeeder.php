23b72ae585c5355dcf33c5cdfd7a2a5ad4fa0d6c
AbdULrahem <abdulrahemalz1@gmail.com>
2022-06-24 17:06:13 +0300

    admin seed

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use phpseclib3\Crypt\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -13,6 +17,12 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admins')->delete();
+        Admin::query()->create([
+            'email' => 'albarakah@gmail.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('albarakah12345@@'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
