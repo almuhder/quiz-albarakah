e4be7ddd088ead1a7ae048d4f19544a4ded3983a
AbdULrahem <abdulrahemalz1@gmail.com>
2022-06-24 19:35:05 +0300

    admin seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(
             ExamTimeSeeder::class,
-           AdminSeeder::class,
-
+        );
+        $this->call(
+            AdminSeeder::class,
         );
     }
 }
