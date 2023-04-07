6e8d6784b1d96e0388e8b1f1d57860b62939b20b
AbdULrahem <abdulrahemalz1@gmail.com>
2023-04-07 21:52:16 +0300

    upgrade laravel version  to 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -20,13 +20,15 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     *
-     * @return void
-     */
+    */
+    public function register(): void
+    {
+        Passport::ignoreRoutes();
+    }
+
     public function boot()
     {
         $this->registerPolicies();
-        Passport::routes();
         Passport::tokensCan([
             'admin' => 'admin',
             'student' => 'student',
